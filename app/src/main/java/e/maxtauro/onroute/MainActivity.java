efc7c367a2dc1ea6720178b3ba1246aea61ebaa7
Max Tauro <maxtauro@hotmail.com>
2018-03-11 20:52:33 -0400

    user stays signed in even if app closes

--- app/src/main/java/e/maxtauro/onroute/MainActivity.java
+++ app/src/main/java/e/maxtauro/onroute/MainActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,36 +32,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.context = this;
 
-        btnEmailLogin = (Button) findViewById(R.id.email_login_button);
-        btnEmailLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast.makeText(context, "clicked email", Toast.LENGTH_SHORT).show();
 
-                startActivityForResult(
-                        AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAllowNewEmailAccounts(true)
-                        .build(), LOGIN_PERMISSION);
-            }
-        });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
 
-        btnFbookLogin = (Button) findViewById(R.id.fb_login_button);
-        btnFbookLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast.makeText(context, "clicked fbook", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, ListFriends.class);
+            startActivity(intent);
+            finish();
 
-                startActivityForResult(
-                        AuthUI.getInstance()
-                                .createSignInIntentBuilder()
-                                .setProviders(providers)
-                                .build(), LOGIN_PERMISSION);
-            }
-        });
+        }
+        else {
 
+            this.context = this;
+
+            btnEmailLogin = (Button) findViewById(R.id.email_login_button);
+            btnEmailLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context, "clicked email", Toast.LENGTH_SHORT).show();
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAllowNewEmailAccounts(true)
+                                    .build(), LOGIN_PERMISSION);
+                }
+            });
+
+            btnFbookLogin = (Button) findViewById(R.id.fb_login_button);
+            btnFbookLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context, "clicked fbook", Toast.LENGTH_SHORT).show();
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setProviders(providers)
+                                    .build(), LOGIN_PERMISSION);
+                }
+            });
+        }
     }
 
     @Override
